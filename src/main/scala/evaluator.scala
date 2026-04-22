ba3c7ef64052d0bd62033cfabbd481ccb381a6e4
kdemchyshyn <kdemchyshyn@kse.org.ua>
2026-04-23 00:51:28 +0300

    Added architecture + API for everyine to start work

---
+++ src/main/scala/evaluator.scala
@@ -0,0 +1,15 @@
+import lambda_terms.Term
+import strategies.Strategy
+
+import scala.annotation.{tailrec, targetName}
+import scala.collection.immutable
+
+object evaluator:
+
+  enum Result:
+    case Success(term: Term)
+    case Timeout(lastTerm: Term)
+  
+  object Evaluator:
+    //@tailrec uncomment when recursion is ready
+    def evaluate(term: Term, strategy: Strategy, limit: Int): Result = ???
---
+++ src/main/scala/strategies.scala
@@ -0,0 +1,14 @@
+import lambda_terms.Term
+
+import scala.annotation.{tailrec, targetName}
+import scala.collection.immutable
+
+object strategies:
+  sealed trait Strategy:
+    def reductionStep(term: Term): Option[Term]
+
+  object NormalOrder extends Strategy:
+    def reductionStep(term: Term): Option[Term] = ???
+
+  object ApplicativeOrder extends Strategy:
+    def reductionStep(term: Term): Option[Term] = ???
