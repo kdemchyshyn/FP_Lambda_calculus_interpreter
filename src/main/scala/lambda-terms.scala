e896494523fbb81ed5bccd69f5dbc732d94a2977
kdemchyshyn <kdemchyshyn@kse.org.ua>
2026-04-23 07:04:06 +0300

    Added implementation for lambda terms

--- src/main/scala/lambda-terms.scala
+++ src/main/scala/lambda-terms.scala
@@ -14,10 +14,26 @@ object lambda_terms:
       if this == variable then replacement
       else this
 
+    def concatenate(symbol: String): Variable = Variable(name + symbol)
+
+  @tailrec
+  def genNewVariable(variable: Variable, forbidden: Set[Variable]): Variable =
+    if forbidden.contains(variable) then genNewVariable(variable.concatenate("'"), forbidden)
+    else variable
+
   case class Abstraction(boundVariable: Variable, body: Term) extends Term:
-    def freeVariables: Set[Variable] = ???
-    def substitute(variable: Variable, replacement: Term): Term = ???
+    def freeVariables: Set[Variable] = body.freeVariables - boundVariable
+
+    def substitute(variable: Variable, replacement: Term): Term =
+      if !freeVariables.contains(variable) then this
+      else if !replacement.freeVariables.contains(boundVariable) then
+        Abstraction(boundVariable, body.substitute(variable, replacement))
+      else
+        val newVariable = genNewVariable(boundVariable, (body.freeVariables ++ replacement.freeVariables))
+        Abstraction(newVariable, body.substitute(boundVariable, newVariable).substitute(variable, replacement))
 
   case class Application(left: Term, right: Term) extends Term:
-    def freeVariables: Set[Variable] = ???
-    def substitute(variable: Variable, replacement: Term): Term = ???
+    def freeVariables: Set[Variable] = left.freeVariables ++ right.freeVariables
+
+    def substitute(variable: Variable, replacement: Term): Term =
+      Application(left.substitute(variable, replacement), right.substitute(variable, replacement))
